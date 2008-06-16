7facd741ba34735771be3f129d257892d4e01909
/C=US/ST=Massachusetts/O=Massachusetts Institute of Technology/OU=Client CA v1/CN=Stuart Pope Layton/emailAddress=slayton@MIT.EDU </C=US/ST=Massachusetts/O=Massachusetts Institute of Technology/OU=Client CA v1/CN=Stuart Pope Layton/emailAddress=slayton@MIT.EDU@5dccc5d9-beb7-0310-bf71-cc18d937a3f4>
2008-06-16 14:25:50 +0000

    un-needed files removed from the debian directory
    git-svn-id: file:///home/jonas/soma/svn-restore.20080710/software/network/trunk@2861 5dccc5d9-beb7-0310-bf71-cc18d937a3f4

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the libsomanetwork package
-#
-0 4	* * *	root	libsomanetwork_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(libsomanetwork):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="libsomanetwork" command="/usr/bin/libsomanetwork"
