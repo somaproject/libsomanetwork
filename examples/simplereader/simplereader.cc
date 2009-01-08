936a946c412d3d9e50d334e630b39153f30ab7cd
Eric Jonas <jonas@mit.edu>
2009-01-08 10:24:54 -0500

    Created simple initial example

---
+++ examples/simplereader/simplereader.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <somanetwork/network.h>
+#include <somanetwork/tspike.h>
+#include <list>
+
+std::string SOMAIP = "10.0.0.2"; 
+
+int main()
+{
+  // Create the soma network object
+  Network net(SOMAIP); 
+  
+  // enable TSPIKE from data source 0
+  net.enableDataRX(0, TSPIKE); 
+
+  // start running
+  net.run(); 
+  
+  int datapipe = net.getDataFifoPipe(); 
+  
+  // create a list to hold our spikes
+  std::list<TSpike_t> tspike_list; 
+
+  // now read 10 packets
+  for (int i = 0; i < 10; i++) {
+    char dummychar; 
+    read(datapipe, &dummychar, 1);  // read a single byte out of the fifo, or block
+    
+    pDataPacket_t dp = net.getNewData(); 
+    
+    // now convert to a tspike packet
+
+    TSpike_t ts = rawToTSpike(dp); 
+    
+    // save the tspike packet for later
+    tspike_list.push_back(ts); 
+  }
+
+  net.shutdown(); 
+
+  std::cout << "We received " << tspike_list.size() 
+	    << " tspikes" << std::endl; 
+
+}
