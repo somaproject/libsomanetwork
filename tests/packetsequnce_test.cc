485603b94c090667915083470124e54fc976b25c
Eric Jonas <jonas@mit.edu>
2009-03-24 09:08:55 -0400

    Added unit test for packetsequence

---
+++ tests/packetsequnce_test.cc
@@ -0,0 +1,18 @@
+
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/auto_unit_test.hpp>
+#include <iostream>
+#include <boost/array.hpp>
+#include <arpa/inet.h>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+#include "somanetwork/packetsequence.h"
+
+using boost::unit_test::test_suite;
+
+BOOST_AUTO_TEST_SUITE(packetsequence) 
+
+BOOST_AUTO_TEST_CASE(simple)
+
+END_BOOST_AUTO_TEST_SUITE();
